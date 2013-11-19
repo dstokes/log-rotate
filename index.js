a70d54f016f105ace525826be689f1575855673e
Drew Stokes <drew@drewstokes.com>
2013-11-18 16:30:40 -0800

    remove sync calls

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ var fs = require('fs')
   , zlib = require('zlib')
   , path = require('path');
 
+function series(jobs, cb) {
+  (function next(err) {
+    if (err) return cb(err);
+    var job = jobs.shift();
+    job ? job(next) : cb(null);
+  })();
+}
+
 function zip(file, cb) {
   fs.createReadStream(file)
     .pipe( zlib.createGzip() )
@@ -38,11 +46,16 @@ module.exports = function(file, options, cb) {
     // reverse sort files by name
     toShift.sort(function(a, b) { return b > a ? 1 : -1; });
 
+    var jobs = [];
+
     // increment each files index
     for (i = 0, l = toShift.length; i < l; i++) {
+      var curr = toShift[i];
       // remove log files outside of the `count` limit
       if (count !== null && l >= count && i <= (l - count)) {
-        fs.unlinkSync(path.join(dir, '/', toShift[i]));
+        jobs.push(function(done) {
+          fs.unlink(path.join(dir, '/', curr), done);
+        });
       } else {
         var parts = toShift[i].split('.');
         // increment the log file index
@@ -52,22 +65,27 @@ module.exports = function(file, options, cb) {
           }
         }
         // move the file to the new index
-        fs.renameSync(
-          path.join(dir, '/', toShift[i]),
-          path.join(dir, '/', parts.join('.'))
-        );
+        jobs.push(function(done) {
+          fs.rename(
+            path.join(dir, '/', curr),
+            path.join(dir, '/', parts.join('.')),
+            done
+          );
+        });
       }
     }
 
-    // move the original log file
-    var rotated = file + '.0';
-    fs.rename(file, rotated, function(err) {
-      // compress the newly rotated file
-      if (options.compress === true) {
-        zip(rotated, cb);
-      } else {
-        cb(null, rotated);
-      }
+    series(jobs, function(err) {
+      // move the original log file
+      var rotated = file + '.0';
+      fs.rename(file, rotated, function(err) {
+        // compress the newly rotated file
+        if (options.compress === true) {
+          zip(rotated, cb);
+        } else {
+          cb(null, rotated);
+        }
+      });
     });
   });
 }
